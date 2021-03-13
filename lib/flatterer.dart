b407ed5f98223448174a9328f28eeae29f141c09
lei.chang <540739522@qq.com>
2021-03-13 16:31:11 +0800

    修改dismiss逻辑

--- lib/flatterer.dart
+++ lib/flatterer.dart
@@ -1,10 +1,13 @@
 library flatterer;
 
+export 'package:flatterer/src/animated_overlay.dart';
 export 'package:flatterer/src/dismiss_window_scope.dart';
 export 'package:flatterer/src/drop_down_action.dart';
 export 'package:flatterer/src/drop_down_menu.dart';
 export 'package:flatterer/src/flatterer_window.dart';
+export 'package:flatterer/src/geometry.dart';
 export 'package:flatterer/src/icon_label.dart';
+export 'package:flatterer/src/indicate_border.dart';
 export 'package:flatterer/src/overlay_window.dart';
 export 'package:flatterer/src/popup_window.dart';
 export 'package:flatterer/src/stack_window.dart';
