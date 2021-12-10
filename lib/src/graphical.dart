532547f7e76945c9a466aba754eac2bd29b1b6cf
changlei <changlei@graspyun.com>
2021-12-10 23:05:21 +0800

    refactor: 支持空安全

---
+++ lib/src/graphical.dart
@@ -0,0 +1,89 @@
+/*
+ * Copyright (c) 2021 CHANGLEI. All rights reserved.
+ */
+
+import 'dart:math' as math;
+
+import 'package:flutter/rendering.dart';
+
+const _topRadius = 2.0;
+const _bottomRadius = 1.5;
+
+/// Created by changlei on 2021/12/10.
+///
+/// 计算各种图形
+/// 三角形
+Path trianglePath(double width, double height, [double radius = 0]) {
+  final compensate = heightCompensate(width, height, radius / _topRadius);
+  return cornerPath(
+    width,
+    compensate,
+    radius,
+  ).shift(Offset(-width / 2, height - compensate));
+}
+
+/// 补偿
+double heightCompensate(double width, double height, [double radius = 0]) {
+  final of = height - radius;
+  final bf = width / 2;
+  final bo = math.sqrt(math.pow(of, 2) + math.pow(bf, 2));
+  final bof = math.atan(bf / of);
+  final boe = math.acos(radius / bo);
+  final newRadians = bof + boe - math.pi / 2;
+  return bf / math.tan(newRadians);
+}
+
+/// 创建元素path
+Rect pathBounds(
+  double radians,
+  double radius, {
+  double rotation = 0,
+  Offset offset = Offset.zero,
+  bool half = false,
+}) {
+  final ae = radius / math.tan(radians);
+  final ag = ae * math.cos(radians);
+  final eg = ae * math.sin(radians);
+  final ai = ae / math.cos(radians) - radius;
+
+  final matrix4 = Matrix4.rotationZ(rotation);
+  final rect = Rect.fromPoints(
+    Offset(-eg, ag),
+    Offset(half ? 0 : eg, half ? ai : ag),
+  );
+  return MatrixUtils.transformRect(matrix4, rect).shift(offset);
+}
+
+/// 创建各个角
+Path cornerPath(double width, double height, double radius) {
+  final radians = math.atan(width / 2 / height);
+
+  final top = pathBounds(
+    radians,
+    radius / _topRadius,
+    rotation: 0,
+    offset: Offset(width / 2, 0),
+    half: true,
+  );
+
+  final left = pathBounds(
+    (math.pi / 2 + radians) / 2,
+    radius * _bottomRadius,
+    rotation: math.pi - (math.pi / 2 - radians) / 2,
+    offset: Offset(0, height),
+  );
+
+  final path = Path();
+  path.moveTo(top.right, top.top);
+  path.arcToPoint(top.bottomLeft, radius: Radius.circular(radius / _topRadius), clockwise: false);
+  path.lineTo(left.right, left.top);
+  path.arcToPoint(left.bottomLeft, radius: Radius.circular(radius * _bottomRadius), clockwise: true);
+  path.lineTo(top.right, left.bottom);
+  path.close();
+
+  return Path.combine(
+    PathOperation.union,
+    path,
+    path.transform(Matrix4.rotationY(math.pi).storage).shift(Offset(width, 0)),
+  );
+}
--- lib/src/indicate_border.dart
+++ lib/src/indicate_border.dart
@@ -1,11 +1,14 @@
 /*
- * Copyright (c) 2020 CHANGLEI. All rights reserved.
+ * Copyright (c) 2021 CHANGLEI. All rights reserved.
  */
 
 import 'dart:math' as math;
 
+import 'package:flatterer/src/graphical.dart' as graphical;
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
+
+const _cornerRadius = 8.0;
 
 /// Created by box on 2020/7/28.
 ///
@@ -13,12 +16,15 @@ import 'package:flutter/foundation.dart';
 class IndicateBorder extends OutlinedBorder {
   /// 带有指示器的自定义border
   const IndicateBorder({
-    this.indicateSize = Size.zero,
-    this.borderRadius = BorderRadius.zero,
-    this.anchor = Rect.zero,
+    Size? indicateSize,
+    BorderRadiusGeometry? borderRadius,
+    Rect? anchor,
     this.direction = Axis.vertical,
     BorderSide side = BorderSide.none,
-  }) : super(side: side);
+  })  : indicateSize = indicateSize ?? Size.zero,
+        borderRadius = borderRadius ?? BorderRadius.zero,
+        anchor = anchor ?? Rect.zero,
+        super(side: side);
 
   /// 三角形的大小，宽高对应三角形的底边和高
   final Size indicateSize;
@@ -54,9 +60,9 @@ class IndicateBorder extends OutlinedBorder {
     if (a is IndicateBorder) {
       return IndicateBorder(
         side: BorderSide.lerp(a.side, side, t),
-        borderRadius: BorderRadiusGeometry.lerp(a.borderRadius, borderRadius, t)!,
-        indicateSize: Size.lerp(a.indicateSize, indicateSize, t)!,
-        anchor: Rect.lerp(a.anchor, anchor, t)!,
+        borderRadius: BorderRadiusGeometry.lerp(a.borderRadius, borderRadius, t),
+        indicateSize: Size.lerp(a.indicateSize, indicateSize, t),
+        anchor: Rect.lerp(a.anchor, anchor, t),
         direction: direction,
       );
     }
@@ -68,9 +74,9 @@ class IndicateBorder extends OutlinedBorder {
     if (b is IndicateBorder) {
       return IndicateBorder(
         side: BorderSide.lerp(side, b.side, t),
-        borderRadius: BorderRadiusGeometry.lerp(borderRadius, b.borderRadius, t)!,
-        indicateSize: Size.lerp(indicateSize, b.indicateSize, t)!,
-        anchor: Rect.lerp(anchor, b.anchor, t)!,
+        borderRadius: BorderRadiusGeometry.lerp(borderRadius, b.borderRadius, t),
+        indicateSize: Size.lerp(indicateSize, b.indicateSize, t),
+        anchor: Rect.lerp(anchor, b.anchor, t),
         direction: direction,
       );
     }
@@ -104,99 +110,64 @@ class IndicateBorder extends OutlinedBorder {
     return _getPath(rect, textDirection: textDirection);
   }
 
-  Path _getVerticalIndicatePath(Rect rect, double offset, double width, double height) {
-    final indicateOriginSize = indicateSize.height;
-    final translation = (anchor.center - rect.centerLeft).dx;
-    Path indicatePath;
-    if (rect.center.dy < anchor.center.dy) {
-      indicatePath = Path()
-        ..moveTo(translation, rect.height - offset + indicateOriginSize)
-        ..relativeLineTo(-width / 2, -height)
-        ..relativeLineTo(width, 0)
-        ..close();
-    } else {
-      indicatePath = Path()
-        ..moveTo(translation, offset - indicateOriginSize)
-        ..relativeLineTo(-width / 2, height)
-        ..relativeLineTo(width, 0)
-        ..close();
+  Path _getIndicatePath(double width, double height, [AxisDirection? direction, Offset? offset]) {
+    var path = graphical.trianglePath(width, height, _cornerRadius);
+    if (direction != null && direction.angle % 360 != 0) {
+      path = path.transform(Matrix4.rotationZ(direction.radians).storage);
     }
-    return indicatePath;
-  }
-
-  Path _getHorizontalIndicatePath(
-    Rect rect,
-    double offset,
-    double width,
-    double height, {
-    TextDirection? textDirection,
-  }) {
-    final indicateOriginSize = indicateSize.height;
-    final translation = (anchor.center - rect.topCenter).dy;
-    final isArrowPointingEnd = rect.center.dx < anchor.center.dx && textDirection == TextDirection.ltr;
-    Path indicatePath;
-    if (isArrowPointingEnd) {
-      indicatePath = Path()
-        ..moveTo(rect.width - offset + indicateOriginSize, translation)
-        ..relativeLineTo(-width, height / 2)
-        ..relativeLineTo(0, -height)
-        ..close();
-    } else {
-      indicatePath = Path()
-        ..moveTo(offset - indicateOriginSize, translation)
-        ..relativeLineTo(width, height / 2)
-        ..relativeLineTo(0, -height)
-        ..close();
+    if (offset != null) {
+      path = path.shift(offset);
     }
-    return indicatePath;
+    return path;
   }
 
   Path _getPath(Rect rect, {TextDirection? textDirection, double delta = 0}) {
     assert(delta >= 0);
-    final path = Path()..addRRect(borderRadius.resolve(textDirection).toRRect(rect).deflate(delta));
-    if (indicateSize.isEmpty) {
-      return path;
-    }
-
     final indicateWidth = indicateSize.width;
     final indicateHeight = indicateSize.height;
 
     double newIndicateWidth;
     double newIndicateHeight;
-    double offset;
+    double newDelta;
     if (indicateHeight == 0) {
       newIndicateWidth = indicateWidth;
       newIndicateHeight = indicateHeight;
-      offset = 0;
+      newDelta = 0;
     } else {
       final radians = math.atan(indicateWidth / 2 / indicateHeight);
-      offset = delta / math.sin(radians);
-      newIndicateHeight = indicateHeight - offset + delta;
+      newDelta = delta / math.sin(radians);
+      newIndicateHeight = indicateHeight - newDelta + delta;
       newIndicateWidth = newIndicateHeight * math.tan(radians) * 2;
     }
 
-    Path indicatePath;
+    final indicateOriginSize = indicateHeight;
+
+    AxisDirection? axisDirection;
+    Offset offset;
     if (direction == Axis.vertical) {
-      indicatePath = _getVerticalIndicatePath(
-        rect,
-        offset,
-        newIndicateWidth,
-        newIndicateHeight,
-      );
+      final translation = (anchor.center - rect.centerLeft).dx;
+      if (rect.center.dy < anchor.center.dy) {
+        axisDirection = AxisDirection.down;
+        offset = Offset(translation, rect.height - newDelta + indicateOriginSize);
+      } else {
+        axisDirection = AxisDirection.up;
+        offset = Offset(translation, newDelta - indicateOriginSize);
+      }
     } else {
-      indicatePath = _getHorizontalIndicatePath(
-        rect,
-        offset,
-        newIndicateHeight,
-        newIndicateWidth,
-        textDirection: textDirection,
-      );
+      final translation = (anchor.center - rect.topCenter).dy;
+      if (rect.center.dx < anchor.center.dx) {
+        axisDirection = AxisDirection.right;
+        offset = Offset(rect.width - newDelta + indicateOriginSize, translation);
+      } else {
+        axisDirection = AxisDirection.left;
+        offset = Offset(newDelta - indicateOriginSize, translation);
+      }
     }
 
     return Path.combine(
       PathOperation.union,
-      indicatePath.shift(rect.topLeft),
-      path,
+      _getIndicatePath(newIndicateWidth, newIndicateHeight, axisDirection, offset + rect.topLeft),
+      Path()..addRRect(borderRadius.resolve(textDirection).toRRect(rect).deflate(delta)),
     );
   }
 
@@ -240,4 +211,23 @@ class IndicateBorder extends OutlinedBorder {
   String toString() {
     return '${objectRuntimeType(this, 'IndicateBorder')}($indicateSize, $borderRadius, $anchor, $direction, $side)';
   }
+}
+
+extension _AxisDirectionExtension on AxisDirection {
+  /// 对应的角度
+  double get angle {
+    switch (this) {
+      case AxisDirection.up:
+        return 0;
+      case AxisDirection.right:
+        return 90;
+      case AxisDirection.down:
+        return 180;
+      case AxisDirection.left:
+        return 270;
+    }
+  }
+
+  /// 弧度
+  double get radians => angle * math.pi / 180;
 }
